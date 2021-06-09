e2d70bad8fef01e691cf9ed9683657b8f661aa1e
Alex <gmreloj@gmail.com>
2021-06-09 16:11:30 +0300

    first commit

--- src/com/androievar/Storage.java
+++ src/com/androievar/Storage.java
@@ -6,7 +6,6 @@ abstract public class Storage {
     public int sumWeight(Item[] array) {
         this.array = array;
         int sumArray = 0;
-
         for (int i = 0; i < array.length; i++) {
             if (array[i] != null) {
                 sumArray = sumArray + array[i].weight;
